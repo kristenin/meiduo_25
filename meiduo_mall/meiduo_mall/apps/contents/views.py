3aae6fd48468fe0841512b3390c8b2a5b60af11b
python <827406976@qq.com>
2019-05-06 15:48:49 +0800

    商品广告数据查询

--- meiduo_mall/meiduo_mall/apps/contents/views.py
+++ meiduo_mall/meiduo_mall/apps/contents/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from goods.models import GoodsChannel, GoodsCategory
 from .utils import get_categories
+from .models import ContentCategory,Content
 
 # Create your views here.
 
@@ -11,8 +12,19 @@ class IndexView(View):
         """
         商品分类及广告数据展示
         """
+        contents = {}   # 用来装所有广告数据的字典
+        """
+        {
+            'index_lbt':lbt_qs,
+            'index_kx':kx_qs
+        }
+        """
+        contentCategory_qs = ContentCategory.objects.all()  # 获取所有广告类别数据
+        for category in contentCategory_qs:
+            contents[category.key] = category.content_set.filter(status=True).order_by('sequence')
 
         context = {
-            'categories':get_categories()
+            'categories':get_categories(),
+            'contents':contents
         }
         return render(request, 'index.html',context)
