4a4a245824ec6eac3054e853e400514135ab1357
python <827406976@qq.com>
2019-05-06 15:17:01 +0800

    首页商品分类封装

--- meiduo_mall/meiduo_mall/apps/contents/views.py
+++ meiduo_mall/meiduo_mall/apps/contents/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from goods.models import GoodsChannel, GoodsCategory
+from .utils import get_categories
 
 # Create your views here.
 
@@ -10,28 +11,8 @@ class IndexView(View):
         """
         商品分类及广告数据展示
         """
-        categories = {}     # 用来包装所有商品类别数据
-        # 获取所有一级类别分组数据
-        goods_channels_qs = GoodsChannel.objects.order_by('group_id', 'sequence')
-        for channel in goods_channels_qs:
-            group_id = channel.group_id # 获取组号
-
-            # 判断当前的组号在字典中是否存在
-            if group_id not in categories:
-                # 不存在，包装一个当前组的准备数据
-                categories[group_id] = {'channels':[], 'cat_subs':[]}
-            cat1 = channel.category # 获取一级类别数据
-            cat1.url = channel.url  # 将频道中的url绑定到一级类型对象
-
-            categories[group_id]['channels'].append(cat1)
-
-            cat2_qs = cat1.subs.all()   # 获取当前一组下面的所有二级数据
-            for cat2 in cat2_qs:    # 遍历二级数据查询集
-                cat3_qs = cat2.subs.all()
-                cat2.cat_subs = cat3_qs # 把二级下面的所有三级绑定给cat2对象的cat_subs属性
-                categories[group_id]['cat_subs'].append(cat2)
 
         context = {
-            'categories':categories
+            'categories':get_categories()
         }
         return render(request, 'index.html',context)
