a5b7ceaa6201b375b2100397ba96687b0fb18fb2
python <827406976@qq.com>
2019-05-11 20:57:03 +0800

    订单提交

--- meiduo_mall/meiduo_mall/apps/orders/urls.py
+++ meiduo_mall/meiduo_mall/apps/orders/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     # 结算订单
     url(r'^orders/settlement/$', views.OrderSettlementView.as_view()),
+
+    # 订单提交
+    url(r'^orders/commit/$', views.OrderCommitView.as_view()),
 ]
--- meiduo_mall/meiduo_mall/apps/orders/views.py
+++ meiduo_mall/meiduo_mall/apps/orders/views.py
@@ -4,6 +4,11 @@ from users.models import Address
 from django_redis import get_redis_connection
 from goods.models import SKU
 from decimal import Decimal
+import json
+from django import http
+from .models import OrderInfo,OrderGoods
+from django.utils import timezone
+from meiduo_mall.utils.response_code import RETCODE
 
 class OrderSettlementView(LoginRequiredView):
     """结算订单"""
@@ -52,3 +57,99 @@ class OrderSettlementView(LoginRequiredView):
             'payment_amount': total_amount + freight  # 实付款
         }
         return render(request, 'place_order.html', context)
+
+class OrderCommitView(LoginRequiredView):
+    """订单提交"""
+
+    def post(self, request):
+
+        # 接收前端传入的收货地址,及支付方式
+        json_dict = json.loads(request.body.decode())
+        address_id = json_dict.get('address_id')
+        pay_method = json_dict.get('pay_method')
+        # 校验
+        if all([address_id, pay_method]) is False:
+            return http.HttpResponseForbidden('缺少必传参数')
+
+        try:
+            address = Address.objects.get(id=address_id)
+        except Address.DoesNotExist:
+            return http.HttpResponseForbidden('收货有误')
+
+        if pay_method not in [OrderInfo.PAY_METHODS_ENUM.get('CASH'), OrderInfo.PAY_METHODS_ENUM.get('ALIPAY')]:
+            return http.HttpResponseForbidden('支付方式有误')
+
+        # 20190511121110 + %09d % user.id
+        user = request.user
+        # 生成订单编号
+        order_id = timezone.localtime().strftime('%Y%m%d%H%M%S') + ('%09d' % user.id)
+
+        # 订单状态
+        status = (OrderInfo.ORDER_STATUS_ENUM.get('UNPAID')
+                  if pay_method == OrderInfo.PAY_METHODS_ENUM.get('ALIPAY')
+                  else OrderInfo.ORDER_STATUS_ENUM.get('UNSEND'))
+
+        # 创建一个订单基本信息模型 并存储
+        order = OrderInfo.objects.create(
+            order_id=order_id,
+            user=user,
+            address=address,
+            total_count=0,
+            total_amount=Decimal('0.00'),
+            freight=Decimal('10.00'),
+            pay_method=pay_method,
+            status=status
+        )
+
+        # 创建redis连接
+        redis_conn = get_redis_connection('carts')
+        # 获取hash数据
+        redis_cart = redis_conn.hgetall('carts_%s' % user.id)
+        # 获取set集合数据
+        cart_selected = redis_conn.smembers('selected_%s' % user.id)
+        cart_dict = {}
+        # 遍历set把要购买的sku_id和count包装到一个新字典中
+        for sku_id_bytes in cart_selected:
+            cart_dict[int(sku_id_bytes)] = int(redis_cart[sku_id_bytes])
+
+        # 遍历用来包装所有要购买商品的字典
+        for sku_id in cart_dict:
+            # 通过sku_id获取到sku模型
+            sku = SKU.objects.get(id=sku_id)
+            # 获取当前商品要购买的数量
+            buy_count = cart_dict[sku_id]
+            # 获取当前商品的库存和销量
+            origin_stock = sku.stock
+            origin_sales = sku.sales
+
+            # 判断库存
+            if buy_count > origin_stock:
+                return http.JsonResponse({'code': RETCODE.STOCKERR, 'errmsg': '库存不足'})
+
+            new_stock = origin_stock - buy_count  # 计算新的库存
+            new_sales = origin_sales + buy_count  # 计算新的销量
+            # 修改sku的库存
+            sku.stock = new_stock
+            # 修改sku的销量
+            sku.sales = new_sales
+            sku.save()
+
+            # 修改spu的销量
+            sku.spu.sales += buy_count
+            sku.spu.save()
+
+            # 存储订单商品记录
+            OrderGoods.objects.create(
+                order=order,
+                sku=sku,
+                count=buy_count,
+                price=sku.price
+            )
+
+            order.total_count += buy_count  # 累加订单商品总数量
+            order.total_amount += (sku.price * buy_count)  # 累加商品总价
+
+        order.total_amount += order.freight  # 累加运费
+        order.save()
+
+        return http.JsonResponse({'code': RETCODE.OK, 'errmsg': '提交订单成功', 'order_id': order_id})
